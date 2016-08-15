ce17d15798c1f3f1cc9df8e3b1580c0e9a0edd30
0x00-pl <0x00.pl@gmail.com>
2016-08-15 22:35:20 +0800

    add error hander

--- index.js
+++ index.js
@@ -3,10 +3,33 @@ var http = require('http'),
     tmp = require('tmp'),
     path = require('path'),
     spawn = require('child_process').spawn,
-    Busboy = require('busboy')
+    Busboy = require('busboy'),
+    inspect = require('util').inspect
 
 
 const redirect_url = '/'
+
+
+// List of HTML entities for escaping.
+var htmlEscapes = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',
+    '/': '&#x2F;'
+};
+
+// Regex containing the keys listed immediately above.
+var htmlEscaper = /[&<>"'\/]/g;
+
+// Escape a string for HTML interpolation.
+var escapeHTML = function(string) {
+    return ('' + string).replace(htmlEscaper, function(match) {
+        return htmlEscapes[match];
+    });
+};
+
 
 function make_cmd(src, dst, extra){
     return ['.\\run_cmd.bat', [src, dst]]
@@ -14,6 +37,14 @@ function make_cmd(src, dst, extra){
 
 function change_ext(filename){
     return filename.substr(0, filename.length-4) + '.svmp.exe'
+}
+
+function error_template(err, stdout, stderr){
+    return '<html><head></head><body>'+
+        '<div class="err">'+escapeHTML(err)+'</div>'+
+        '<div class="stdout">'+escapeHTML(stdout)+'</div>'+
+        '<div class="stderr">'+escapeHTML(stderr)+'</div>'+
+        '</body></html>'
 }
 
 function upload_file(req, res){
@@ -25,15 +56,20 @@ function upload_file(req, res){
     const tmp_src_path = tmp.tmpNameSync({postfix:'.exe'})
     const tmp_dst_path = change_ext(tmp_src_path)   // tmp.tmpNameSync()
     var _filename
+    var _extera_args = {}
     var busboy = new Busboy({headers: req.headers})
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+        console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+        _extera_args[fieldname] = inspect(val)
+    });
     busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
         _filename = filename
         const tmp_upload = fs.createWriteStream(tmp_src_path)
-        console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+        console.log('[debug] File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
         file.pipe(tmp_upload)
     })
     busboy.on('finish', function(){
-        const [cmd, args] = make_cmd(tmp_src_path, tmp_dst_path)
+        const [cmd, args] = make_cmd(tmp_src_path, tmp_dst_path, _extra_args)
         console.log('sec,dst', cmd, args)
         var proc = spawn(cmd, args, {stdio: 'inherit'})
         proc.on('close', function(code){
@@ -42,6 +78,9 @@ function upload_file(req, res){
             res.setHeader('Content-type', 'application/octet-stream')
             fs.createReadStream(tmp_dst_path).pipe(res)
         })
+        proc.on('error', function(err){
+            res.end(error_template(err, "", ""))
+        })
     })
     req.pipe(busboy)
 }
@@ -49,7 +88,15 @@ function upload_file(req, res){
 function index(){
     return '<html><head></head><body>'+
         '<form method="POST" enctype="multipart/form-data" action="/upload">'+
-        '<input type="text" name="textfield"><br />'+
+        '<input type="text" name="RI" value="256" /><br />'+
+        '<input type="text" name="NE" value="0" /><br />'+
+        '<input type="text" name="IE" value="1" /><br />'+
+        '<input type="text" name="ID" value="-1" /><br />'+
+        '<input type="text" name="GI" value="-1" /><br />'+
+        '<input type="text" name="CD" value="-1" /><br />'+
+        '<input type="text" name="BD" value="-1" /><br />'+
+        '<input type="text" name="VT" value="0" /><br />'+
+        '<input type="text" name="TC" value="45" /><br />'+
         '<input type="file" name="filefield"><br />'+
         '<input type="submit">'+
         '</form>'+
